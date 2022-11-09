1172fe1b037a01e3f5242bf0459fa2e435a4aa17
mattleesounds <mattleesounds@gmail.com>
2022-11-08 20:19:21 -0600

    init

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Menu from './Menu.svelte' 
+  import Whitepaper from './Whitepaper.svelte'
+  import Contact from './Contact.svelte'
+</script>
+
+<main>
+  <Menu/>
+  <Whitepaper/>
+  <Contact/>
+</main>
+
+<style>
+  
+</style>
---
+++ src/Whitepaper.svelte
@@ -0,0 +1,295 @@
+<div>
+    <h1>POLP White Paper</h1>
+    <h2>Abstract</h2>
+    <p>
+        POLP is an application to stream user-generated audio content that
+        incentivizes active music discovery by offering listeners the chance to
+        become a part of the communities of the artists they support. Artists
+        can easily research and interact with their communities through the
+        community management tool. Artists and fans can exchange value with each
+        other directly through their crypto wallets.
+    </p>
+    <div class="ss" />
+    <h2>Problems</h2>
+    <h3>Problem 1: Market Saturation</h3>
+    <h4>High Release Volume</h4>
+    <p>
+        60,000 songs are released on Spotify every day. <b
+            >As creating and consuming music becomes ever more accessible,
+            competition among musicians will continue increase.</b
+        > Even if filtering out low effort releases, there is an impossible amount
+        of music for fans to listen to. This environment is overwhelming for fans
+        and recording artists, who face endless competition from other upcoming talent
+        and all other previously created recordings. The odds are becoming ever-increasingly
+        stacked against musicians. At the heart of this problem is that consuming
+        music takes up the listeners' valuable time, of which they are not rewarded
+        for beyond discovering new music.
+    </p>
+    <div class="ss" />
+    <h4>Algorithmic Discovery</h4>
+    <p>
+        The over-saturation of music releases has caused algorithmic discovery
+        to become the primary method of finding new music. Services like the
+        Spotify Discover playlist, TikTok, and YouTube recommendations use the
+        data they have about you to predict what you might like and serve it.
+        This can be helpful for many artists and listeners, but there are
+        problems. It means artists are forced to create with the algorithm in
+        mind. They must skew their creations to what an algorithm might promote.
+    </p>
+    <div class="ss" />
+    <p>
+        Since so much of this algorithmic discovery happens on social media,
+        artists also must keep up a strong social media presence. While some
+        artists enjoy posting on social media and find it works well for them,
+        others feel that they are forced to broadcast themselves in a way that
+        they don't enjoy or feel comfortable with. Many do not want to
+        constantly make TikToks and other forms of social media content. This
+        type of content creation also takes valuable time away from the artist
+        that they could otherwise use to fulfill their primary goal of making
+        quality music.
+    </p>
+    <p>
+        Algorithmic discovery can be bad for listeners as well, because it is
+        based on the users' past interests which does not tend to serve new
+        kinds of content. <b
+            >Putting this all together, you get a music discovery landscape that
+            is robotic and uninspired, with artists chasing the algorithm and
+            listeners being passively fed content.</b
+        >
+    </p>
+    <h3>Problem 2: Lack of Community Tools</h3>
+    <p>
+        <b
+            >There has never been a good way for artists and fans to interact
+            directly with each other digitally.</b
+        > It is extremely difficult for artists to keep track of their community
+        and distinguish their earliest and most loyal fans. They are also relinquishing
+        full control of these relationships to third parties.
+    </p>
+    <div class="ss" />
+    <p>
+        For social media services such as Instagram, TikTok, and Twitter,
+        interaction is happening on the servers of that company, and all data is
+        owned by them. If users want to leave the platform, they cannot take
+        their connections or content with them because it is not theirs, it
+        belongs to the company. The social media company is simply allowing them
+        to use it. On POLP, the user can download their data whenever they want,
+        and connections are made on a credibly neutral network: the Ethereum
+        blockchain.
+    </p>
+    <div class="ss" />
+    <h2>Solution</h2>
+    <h3>Incentivised Active Discovery</h3>
+    <p>
+        <b
+            >POLP (Proof of Listenership Platform) solves these problems through
+            the incentivisation of active music discovery by giving listeners a
+            chance to be rewarded for their time investment in listening to new
+            music.</b
+        > Listening on POLP makes them a part of that artist's direct community and
+        gives fans the exclusive perks that come with it. For any successful artist,
+        they would not have been able to get a million fans with out getting 100
+        first. Those first 100 fans are immensely valuable to an artists career.
+        In the current landscape, those early supporters are generally not directly
+        rewarded in any way for their time and loyalty. POLP allows artists to support
+        their earliest fans by creating a wallet to wallet connection between them
+        on the Ethereum blockchain. The artist, equipped with the wallet addresses
+        and other data provided by their earliest and most loyal supporters, is able
+        to reward their fans perpetually throughout their career. This ability to
+        reward listeners comes from the direct transfer of value in the form of tokens
+        that blockchain technology enables. The artist can send tokens representing
+        any type of value that can conceivably be programmed to their loyal listeners,
+        thus giving back to those to whom they owe their career. This connection
+        also enables donations and communication in the other direction: from fan
+        to artist.
+    </p>
+    <h3>Feed</h3>
+    <img src="public/POLP-feed.png" alt="POLP feed user interface" />
+    <p>
+        In this first iteration of our user interface, there are 4 songs at
+        different stages in the process of proving listenership. "Ode to
+        Community" has not been played yet. "Orange Song" is playing currently.
+        "Blockchain Love" has been played but listenership is not yet proven,
+        and "Summer in Nola" has been played and listenership has been proven.
+        <b
+            >Once listenership has been proven, the listener will be part of
+            that artist's community.</b
+        >
+    </p>
+    <div class="ss" />
+    <h3>Community Management Tool</h3>
+    <img src="public/POLP-cmt.png" alt="POLP community management too UI" />
+    <p>
+        The artist can manage their community with the community management tool
+        (CMT). <b
+            >The CMT is an easy-to-use database that gives artists an
+            unprecedented amount of data about their earliest and most loyal
+            fans.</b
+        >
+        This data will include wallet addresses, email addresses, phone numbers,
+        their school/work, favorite venues, which of your songs they have listened
+        to at what time and how often, how much they've donated, what other social
+        media platforms they follow the artist on, their days of availability and
+        any other information that the fan chooses to share with the artist. However,
+        the fan will not be obligated to share any information besides either their
+        wallet or email. Artists can easily sort their fans by early adopters, most
+        listens, location, and other parameters.
+    </p>
+    <div class="ss" />
+    <p>
+        <b
+            >The CMT will also include functionality to interact and exchange
+            value directly with fans.</b
+        > Artists will easily be able to send tokens and directly message their fans.
+        These tokens can represent any type of value that can conceivably be programmed
+        into them, including tickets, discounts on merchandise, and art NFTs; the
+        possibilities of tokenization are expected to expand rapidly as blockchain
+        technology matures. Artists and fans can easily message each other. Artists
+        will be able to easily create and send polls to their listeners to obtain
+        any other information that might be beneficial.
+    </p>
+    <div class="ss" />
+    <h3>More On Incentivised Discovery</h3>
+    <p>
+        <b
+            >At the heart of POLP's model is a behavioral switch in listeners.
+            Going from passive music discovery enabled by algorithms to
+            non-algorithmic active music discovery.</b
+        > Algorithmic music discovery is more convenient because songs are served
+        to listeners with out having to seek them out. Because these songs are recommended
+        by inputing their recent listens into a machine learning algorithm, there
+        is a high likelihood that they will enjoy what is recommended. The negative
+        aspect of algorithmic discovery is that it does not tend to encourage listeners
+        to branch out and listen to new types of music. Also, there are biases that
+        get unintentionally embedded in these systems due to the complex nature of
+        machine learning, which results in some songs getting arbitrarily recommended
+        often, while others are arbitrarily not recommended at all.
+    </p>
+    <div class="ss" />
+    <p>
+        Active music discovery is rare these days because it is time consuming.
+        Using this discovery method takes longer to discover music that the
+        listener loves in comparison to algorithmic discovery. <b
+            >POLP remedies this problem by incentivizing active music discovery
+            through the possibility of being rewarded if an artist you listen to
+            goes on to build a successful career.</b
+        >
+    </p>
+    <div class="ss" />
+    <p>
+        Before machine learning, active music discovery was one of the main ways
+        that people found new music. This would take place in record stores.
+        Digging through crates of physical records, whether it was vinyl, tapes,
+        or CDs. The one caveat to this record store analogy is that all the
+        music there has been deemed good enough to include in the store, either
+        by a record label or the store itself.
+    </p>
+    <div class="ss" />
+    <p>
+        As POLP becomes saturated with music uploads, we will introduce more
+        ways of making it easier to find great new music. This will start with
+        simple ways of organizing music into categories based on time uploaded,
+        popularity, genre, and artist location. <b
+            >Eventually, recommendation algorithms must be introduced, but
+            unlike the systems that are currently used in all popular streaming
+            applications, POLP's algorithms will be transparent and
+            customizable.</b
+        >
+        Users will know how songs are being recommended to them and change it to
+        their liking. For instance, a user will be able to increase the likelihood
+        of being served a certain genre or songs by artists based in a certain location.
+        <b
+            >Because POLP is not owned or affiliated with major record labels,
+            we are not obligated to favor certain artists and can be transparent
+            with our recommendation systems.</b
+        > Other streaming services, such as Spotify, cannot be transparent or customizable
+        because they are forced to favor major label artists, especially ones who
+        are being promoted in their algorithm through closed-door deals, which of
+        course, they do not want the consumer to know about.
+    </p>
+    <div class="ss" />
+    <h3>Accessability For Non-Web3 Natives</h3>
+    <p>
+        <b
+            >A significant portion of POLP's functionality requires an Ethereum
+            wallet, yet most artists and fans do not have one. Most of them do
+            not even know what a self-custody wallet is and many even have a
+            negative perception of crypto generally. To leave these folks out
+            would be inconsistent with our mission.</b
+        > The platform must be useable with out a wallet, and we must educate users
+        on the benefits of getting one, gently encouraging them to embrace web3.
+        Although we believe that signing in with Ethereum and direct wallet connections
+        are the future, we are not in the future yet. We must meet our customers
+        where they are before we can guide them to the promised land.
+    </p>
+    <div class="ss" />
+    <p>
+        To make the app useful to those without a crypto wallet, users must be
+        given the option to sign in with email in the same way they would in a
+        web2 application such as Instagram. There must also be a way to form
+        connections with artists without use of the blockchain. This can be done
+        in a very similar way to using wallets, with the main difference being
+        that the artist sees just email and other data provided by the user
+        without the wallet address included. There will also be the ability to
+        follow other users, allowing you to easily keep track of their releases
+        and listens, and to quickly access their profiles.
+    </p>
+    <h4>Web3 Onboarding</h4>
+    <p>
+        When a user connects with an artist they really like, they will be
+        motivated to create a wallet and connect it to their profile in order to
+        reap all possible benefits from the connection. On the artist side, they
+        too can create their profile with out a wallet address and access their
+        listener data. They will be incentivized to create a wallet in order to
+        receive donations and reward fans. In addition, listeners are more
+        likely to support an artist with a wallet since they are more likely to
+        reward and interact with them.
+    </p>
+    <div class="ss" />
+    <h2>Conclusion</h2>
+    <p>
+        Thank you for reading our white paper. We invite any one who wants to
+        learn more or become an early adopter of our platform to reach out to us
+        through Twitter or Email.
+    </p>
+    <div class="ss" />
+    <div class="ss" />
+</div>
+
+<style>
+    h1 {
+        font-size: 3em;
+        line-height: 1;
+        padding: 100px;
+        padding-top: 50px;
+        padding-bottom: 50px;
+    }
+    h2 {
+        font-size: 2.5em;
+        color: #ffac33;
+    }
+    h3 {
+        font-size: 2.3em;
+        padding: 10px;
+        margin: 10px;
+    }
+    h4 {
+        font-size: 2em;
+        padding: 10px;
+        margin: 10px;
+    }
+    p {
+        text-align: left;
+        margin: 40px;
+        margin-top: 0;
+    }
+    img {
+        width: 75%;
+    }
+    .space {
+        height: 25px;
+    }
+    .ss {
+        height: 10px;
+    }
+</style>
